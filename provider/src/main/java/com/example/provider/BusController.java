80c7a939307de2df034f34e6acb027258adc4d0b
Natnaree Asavaseri <nasavas@ap.xom.com>
2020-05-20 22:50:56 +0700

    add provider application

---
+++ provider/src/main/java/com/example/provider/BusController.java
@@ -0,0 +1,24 @@
+package com.example.provider;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+
+@RestController
+public class BusController {
+    @RequestMapping("/bus/{station}/{nr}")
+    public BusDto bus(@PathVariable String station, @PathVariable int nr) {
+        int eta = getEtaBasedOnGpsAndOtherAdvancedStuff();
+        BusDto b = new BusDto(station, nr, eta);
+        return b;
+    }
+
+    private int getEtaBasedOnGpsAndOtherAdvancedStuff() {
+        Random rn = new Random();
+        int max = 7;
+        int min = 1;
+        return rn.nextInt(max - min +1) + min;
+    }
+}
