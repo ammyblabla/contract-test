69262893bc59e4f7d27d51e219b26a4a0afcd0aa
Natnaree Asavaseri <nasavas@ap.xom.com>
2020-05-25 00:13:03 +0700

    add pact provider

---
+++ provider/src/test/java/com/example/provider/ProviderTest.java
@@ -0,0 +1,38 @@
+package com.example.provider;
+
+import au.com.dius.pact.provider.junit.Consumer;
+import au.com.dius.pact.provider.junit.Provider;
+import au.com.dius.pact.provider.junit.State;
+import au.com.dius.pact.provider.junit.VerificationReports;
+import au.com.dius.pact.provider.junit.loader.PactFolder;
+import au.com.dius.pact.provider.junit5.HttpTestTarget;
+import au.com.dius.pact.provider.junit5.PactVerificationContext;
+import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+
+@Provider("busProvider")
+@Consumer("busConsumer")
+@ExtendWith(SpringExtension.class)
+@PactFolder("pacts")
+public class ProviderTest {
+    @BeforeEach
+    void setUp(PactVerificationContext context) {
+        context.setTarget(new HttpTestTarget("localhost", 8111, "/"));
+    }
+
+    @TestTemplate
+    @ExtendWith(PactVerificationInvocationContextProvider.class)
+    void testTemplate(PactVerificationContext context) {
+        context.verifyInteraction();
+    }
+
+    @State("get bus")
+    public void getBus() {
+    }
+
+
+}
