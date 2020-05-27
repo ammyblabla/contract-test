cf94f48d9cac2ccde8c41663d12e068bf19a7473
Natnaree Asavaseri <nasavas@ap.xom.com>
2020-05-27 17:54:31 +0700

    reuse unused file

--- provider/src/main/java/com/example/provider/BusController.java
+++ provider/src/main/java/com/example/provider/BusController.java
@@ -4,14 +4,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Random;
-
 @RestController
 public class BusController {
     @RequestMapping("/bus/{station}/{nr}")
     public BusDto bus(@PathVariable String station, @PathVariable int nr) {
+        System.out.println("GET /bus/" + station + "/" + nr);
         int eta = nr + 1;
-        BusDto b = new BusDto(station, nr, eta);
-        return b;
+        return new BusDto(station, nr, eta);
     }
 }
